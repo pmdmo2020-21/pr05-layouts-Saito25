f9fd0e047085d06c0f1f84eda49397f50e4be78e
Manuel <ctrltecnico-manuel@hotmail.com>
2020-10-24 17:04:32 +0200

    Fin version 1.0

--- app/src/main/java/es/iessaladillo/pedrojoya/layouts/MainActivity.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/layouts/MainActivity.kt
@@ -2,12 +2,23 @@ package es.iessaladillo.pedrojoya.layouts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import es.iessaladillo.pedrojoya.layouts.databinding.ConstraintlayoutActivityBinding
+import es.iessaladillo.pedrojoya.layouts.databinding.LinearlayoutActivityBinding
+import es.iessaladillo.pedrojoya.layouts.databinding.RelativelayoutActivityBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // Descomentar según la actividad que se quiera ver
+    private lateinit var binding: LinearlayoutActivityBinding
+    //private lateinit var binding: RelativelayoutActivityBinding
+    //private lateinit var binding: ConstraintlayoutActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // TODO: USAR UNO DE LOS LAYOUTS DEFINIDOS PARA PROBARLO.
-        setContentView(R.layout.linearlayout_activity)
+        binding = LinearlayoutActivityBinding.inflate(layoutInflater)
+        //binding = RelativelayoutActivityBinding.inflate(layoutInflater)
+        //binding = ConstraintlayoutActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
